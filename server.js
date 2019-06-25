1370c3260e6e3df1ac79843cd6b7cd778b4cf7d4
JoeySuc89 <jsucameli1@gmail.com>
2019-06-24 21:54:00 -0400

    added a few more files to be workied on to make the project work

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var axios = require('axios');
+var cheerio = require('cheerio');
+
+var db = require('./models');
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+
+mongoose.connect(MONGODB_URI);
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function ()v{
+  res.render('home');
+});
+
+app.listen(PORT, function(){
+  console.log('App now listening at localhost:' + PORT)
+});
